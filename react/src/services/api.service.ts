e18c2005d3c4ec5b7e679a02bf204ac9783e6915
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2026-01-16 18:45:37 +0100

    remove unused imports and files

--- react/src/services/api.service.ts
+++ react/src/services/api.service.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosError, type AxiosRequestConfig, type CreateAxiosDefaults} from "axios";
+import axios, {AxiosError} from "axios";
 import {config} from "../config.ts";
 import {useAuthState} from "../hooks/AuthState.ts";
 import {navigateTo} from "../hooks/Navigation.ts";
